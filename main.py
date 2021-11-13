6d118f09348e6462b5e69ec1c29f18a9c1733f74
Martina Halasz <h.martinaa@gmail.com>
2021-11-13 17:26:39 +0100

    fixed bug with counting

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import re
 FILENAME = '/home/marthalasz/Development/data/CARPATGRID_TA/CARPATGRID_TA_Y.ser'
 TARGET_FILENAME = 'output.txt'
 
-i = 0
 with open(TARGET_FILENAME, 'w') as target_file:
     target_file.write('year;id;value\n')
+
+i = 0
 with open(FILENAME) as file:
     for line in file:
         if i == 0:
@@ -17,6 +18,6 @@ with open(FILENAME) as file:
             year = data_line[0]
             data_list = data_line[1:-1]
             with open(TARGET_FILENAME, 'a') as target_file:
-                for i in range(0, len(header)):
-                    target_file.write(year + ';' + header[i] + ';' + data_list[i] + '\n')
+                for n in range(0, len(header)):
+                    target_file.write(year + ';' + header[n] + ';' + data_list[n] + '\n')
         i = i + 1
